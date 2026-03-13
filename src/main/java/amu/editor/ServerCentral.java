f87b994eeb14aa91a50f2e77dbe289932e7d240e
saturnin <saturnin.kule-kyakakala@etu.univ-amu.fr>
2026-03-13 17:46:38 +0100

    ServerCentral debut

---
+++ src/main/java/amu/editor/ServerCentral.java
@@ -0,0 +1,51 @@
+package amu.editor;
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ServerCentral {
+
+    private static AtomicInteger nombreClients = new AtomicInteger(0);
+    private static AtomicInteger nbthread =  new AtomicInteger(5);
+
+    public static void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(1234)) {
+
+            ExecutorService pool = Executors.newFixedThreadPool(nbthread.get());
+
+            while (true) {
+                Socket client = server.accept();
+                int total = nombreClients.incrementAndGet();
+                System.out.println("Client connecté. Total = " + total);
+                pool.execute(() -> gererClient(client));
+            }
+        } catch (IOException e) {
+            System.out.println("Erreur de connexion");
+        }
+    }
+    private static void gererClient(Socket client) {
+        if (nombreClients.get() == nbthread.get()) {
+            System.out.println("nbtotal");
+        }
+        try {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(client.getInputStream())
+            );
+
+            while (in.readLine() != null) {
+                System.out.println("zoba zoba");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Client déconnecté");
+        } finally {
+            int total = nombreClients.decrementAndGet();
+            System.out.println("Client parti. Total = " + total);
+            try {
+                client.close();
+            } catch (IOException ignored) {}
+        }
+    }
+}
